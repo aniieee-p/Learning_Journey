04e02be16e93a56f2f0fe8840e3314325fecfd77
Anisha Patel <anisha63911@gmail.com>
2026-07-05 17:25:43 +0530

    Time: 623 ms (50.14%), Space: 0B (100%) - LeetHub

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,11 @@
+select tweet_id
+from Tweets
+where CHAR_LENGTH(content) > 15
+
+# CHAR_LENGTH TELLS IN CHAR
+# LENGTH() IN BYTES
+
+-- # Write your MySQL query statement below
+-- select tweet_id
+-- from Tweets
+-- where tweet_id = 2;
