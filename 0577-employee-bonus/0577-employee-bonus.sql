6a9c0a8c481157f95a3806161a3b48af37fef060
Anisha Patel <anisha63911@gmail.com>
2026-07-31 22:42:06 +0530

    Time: 1257 ms (14.92%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -4,25 +4,3 @@ from Employee e
 left join bonus b
 on e.empID = b.empID
 where b.bonus is null or b.bonus < 1000;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- select e.name, b.bonus
--- from Employee e
--- left join Bonus b
--- on e.empID = b.empID
--- where b.bonus is null or b.bonus < 1000;
